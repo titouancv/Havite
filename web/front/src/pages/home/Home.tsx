1135cc86ac03de19521588d80a3474f7e2aa70d6
titouancv <titouan.carion.vignaud@gmail.com>
2025-08-13 19:30:54 +0200

    wip

--- web/front/src/pages/home/Home.tsx
+++ web/front/src/pages/home/Home.tsx
@@ -1,16 +1,25 @@
+import { useState } from 'react'
 import styles from './home.module.scss'
 import Header from '../shared/components/header/Header'
 import Footer from '../shared/components/footer/Footer'
 import Latestrecap from './components/latest-recap/LatestRecap'
+import Modal from '../shared/components/modal/Modal'
 
 function Home() {
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
     <div className={styles.homeContainer}>
       <div className={styles.homeWrapper}>
         <Header />
+        <button onClick={() => setIsOpen(true)}>Open Modal</button>
         <div className={styles.home_body}>
           <h1>Quoi de neuf ?</h1>
           <Latestrecap />
+          <Modal isOpen={isOpen} onClose={() => setIsOpen(false)} title="Oups...">
+            <h2>Modal Title</h2>
+            <p>This is a modal using a separate CSS file.</p>
+          </Modal>
         </div>
         <Footer />
       </div>
