614619d687934408cfa73607559430d7275acff7
titouancv <titouan.carion.vignaud@gmail.com>
2025-08-14 01:12:02 +0200

    mini fix

--- web/front/src/pages/shared/components/header/Header.tsx
+++ web/front/src/pages/shared/components/header/Header.tsx
@@ -1,13 +1,13 @@
 import styles from './header.module.scss'
-import react from '../../../../assets/react.svg'
+import logoMonogram from '../../../../assets/logoMonogram.svg'
 import { Link } from '@tanstack/react-router'
 
 function Header() {
   return (
     <div className={styles.headerContainer}>
       <Link to="/">
-        <div className={styles.logo}>
-          <img src={react} alt="React logo" />
+        <div className={styles.homeButton}>
+          <img className={styles.logo} src={logoMonogram} alt="Havite logo" />
           <h1>HAVITE</h1>
         </div>
       </Link>
