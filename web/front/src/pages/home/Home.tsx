e6ea37ade282a0236ca213d8f22d5947d60fe0c1
titouancv <titouan.carion.vignaud@gmail.com>
2025-08-14 18:04:21 +0200

    wip

--- web/front/src/pages/home/Home.tsx
+++ web/front/src/pages/home/Home.tsx
@@ -1,20 +1,13 @@
 import styles from './home.module.scss'
-import Header from '../shared/components/header/Header'
-import Footer from '../shared/components/footer/Footer'
 import Latestrecap from './components/latest-recap/LatestRecap'
+import PageContainer from '../shared/components/page-container/PageContainer'
 
 function Home() {
   return (
-    <div className={styles.homeContainer}>
-      <div className={styles.homeWrapper}>
-        <Header />
-        <div className={styles.home_body}>
-          <h1>Quoi de neuf ?</h1>
-          <Latestrecap />
-        </div>
-        <Footer />
-      </div>
-    </div>
+    <PageContainer>
+      <h1 className={styles.home_title}>Quoi de neuf ?</h1>
+      <Latestrecap />
+    </PageContainer>
   )
 }
 
--- web/front/src/pages/not-found/NotFound.tsx
+++ web/front/src/pages/not-found/NotFound.tsx
@@ -1,19 +1,14 @@
 import styles from './not-found.module.scss'
-import Header from '../shared/components/header/Header'
-import Footer from '../shared/components/footer/Footer'
+import PageContainer from '../shared/components/page-container/PageContainer'
 
 function NotFound() {
   return (
-    <div className={styles.notFoundContainer}>
-      <div className={styles.notFoundWrapper}>
-        <Header />
-        <div className={styles.notFound_body}>
-          <h1>404</h1>
-          <p>Il semblerait que cette page n’existe pas.</p>
-        </div>
-        <Footer />
+    <PageContainer>
+      <div className={styles.notFound}>
+        <h1>404</h1>
+        <p>Il semblerait que cette page n’existe pas.</p>
       </div>
-    </div>
+    </PageContainer>
   )
 }
 
